26367f55f797baa50b93f84c9771988fffac5c01
Bruna Cotrim <bruna_viotto@hotmail.com>
2022-11-06 22:23:23 -0300

    :sparkles: feat: limita a quantidade de requisições na API

--- website/website/settings.py
+++ website/website/settings.py
@@ -134,4 +134,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '5/minute',
+        'user': '15/minute'
+    },
 }
