f23ee5b813e45de4f3f60c4ba909141aef57e490
Bruna Cotrim <bruna_viotto@hotmail.com>
2022-11-13 10:17:25 -0300

    :books: docs: adiciona documentação Swagger com drf_yasg

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,14 +14,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from users.urls import router
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API WebSite",
+      default_version='v1',
+   ),
+   public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,4 +38,8 @@ urlpatterns = [
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
