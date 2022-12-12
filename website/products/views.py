dcef8121881168c3f2794a25907248c050fb777f
Bruna Cotrim <bruna_viotto@hotmail.com>
2022-12-12 18:58:58 -0300

    :sparkles: feat: adiciona endpoint para consulta da cubagem de produto

--- website/products/views.py
+++ website/products/views.py
@@ -1,10 +1,16 @@
 from .serializers import ProductSerializer
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from django_filters import rest_framework as filters
 from rest_framework.filters import OrderingFilter
+from decimal import Decimal
 
 from .models import Product
 from .filters import ProductFilter
+
+
+CUBIC_FACTOR = Decimal('6000.0')
 
 
 class ProductViewSet(viewsets.ModelViewSet):
@@ -24,3 +30,20 @@ class ProductViewSet(viewsets.ModelViewSet):
         'updated_at',
         'special_price'
         ]
+        
+
+    @action(detail=True, methods=['get'], url_path='cubage', url_name='cubage')
+    def cubage(self, request, pk=None):
+        data = self.get_object()
+        cubed_weight = (data.height * data.width * data.length) / CUBIC_FACTOR
+
+        if cubed_weight > data.weight:
+            cubage = cubed_weight
+        else:
+            cubage = data.weight
+
+        return Response({'cubage': round(cubage, 3),
+                        'cubed_weight': round(cubed_weight, 3),
+                        'real_weight': data.weight,
+                        'cubic_factor': CUBIC_FACTOR})
+    
